6e206a2d9553ac50334d53732964dc98c2fb61fb
=bibin thomas <bibinthomas951@gmail.com>
2022-05-22 19:32:33 +0530

    Main Javascript file

--- script.js
+++ script.js
@@ -4,3 +4,4 @@ const gameBoard = document.getElementById("game-board");
 const grid = new Grid(gameBoard);
 
 grid.radomEmptyCell().tile = new Tile(gameBoard)
+grid.radomEmptyCell().tile = new Tile(gameBoard)
